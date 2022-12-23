4675cc3803a5b5234744a0319315201d07f3367e
Mateusz Zagrzejewski <myemail@myemail.com>
2022-12-23 11:04:34 +0000

    Solved.

--- src/main/java/extendingoutputstream/QuestionFileInputStream.java
+++ src/main/java/extendingoutputstream/QuestionFileInputStream.java
@@ -1,7 +1,43 @@
 package extendingoutputstream;
 
-public class QuestionFileInputStream  {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class QuestionFileInputStream implements AmigoOutputStream {
 
+    private final AmigoOutputStream amigoOutputStream;
+    private final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+    public QuestionFileInputStream(AmigoOutputStream amigoOutputStream) {
+        this.amigoOutputStream = amigoOutputStream;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        amigoOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        amigoOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Do you really want to close the stream? Y/N");
+        if (bufferedReader.readLine().equals("Y")) amigoOutputStream.close();
+    }
 }
+
+
