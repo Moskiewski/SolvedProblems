e5476470770998a3fb365bcee299f4bc92e7ccbd
Mateusz Zagrzejewski <myemail@myemail.com>
2023-01-05 12:05:17 +0000

    Solved.

---
+++ src/main/java/filesAndExceptions/Solution.java
@@ -0,0 +1,17 @@
+package filesAndExceptions;
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String fileName = reader.readLine();
+            try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+            } catch (FileNotFoundException e) {
+                System.out.println(fileName);
+                break;
+            }
+        }
+    }
+}
